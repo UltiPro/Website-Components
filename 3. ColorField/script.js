f53a0d20aca088ed74116806ebdc97902bf98d8d
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-01-09 23:43:05 +0100

    Update

---
+++ 3. ColorField/script.js
@@ -0,0 +1,45 @@
+const box = document.querySelector('.box');
+
+const squares = 378;
+let sliderValue = 70;
+let range = 360;
+
+const createSquares = () => {
+    for (let i = 0; i < squares ; i++) {
+        const square = document.createElement('div');
+
+        square.classList.add('square');
+
+        square.addEventListener('mouseover', () => {
+            setColor(square);
+        });
+
+        square.addEventListener('mouseout', () => {
+            removeColor(square);
+        });
+
+        box.appendChild(square);
+    }
+}
+
+const setColor = square => {
+    let h;
+
+    if(range === 360){
+        h = Math.floor(Math.random() * 360);
+    }
+    else{
+        h = Math.floor(Math.random() * 60) + range;
+    }
+
+    const s = sliderValue + '%';
+    const l = '50%';
+
+    square.style.backgroundColor = `hsl(${h},${s},${l})`;
+}
+
+const removeColor = square => {
+    square.style.backgroundColor = `transparent`;
+}
+
+createSquares();
