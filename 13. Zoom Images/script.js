3825981aa56f405f8dde0e941962aa2e25be19e1
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-01-11 00:19:19 +0100

    Update

---
+++ 13. Zoom Images/script.js
@@ -0,0 +1,19 @@
+const img = document.querySelector('img');
+
+const zoomImg = e => {
+	const x = e.clientX;
+	const y = e.clientY;
+
+	const newX = (img.offsetLeft - x) * (-1);
+	const newY = (img.offsetTop - y) * (-1);
+
+	img.style.transformOrigin = `${newX}px ${newY}px`;
+	img.classList.add('zoom-img');
+}
+
+const resetImg = () =>{
+	img.classList.remove('zoom-img');
+}
+
+img.addEventListener('mousemove', zoomImg);
+img.addEventListener('mouseout', resetImg);
